7672b0bfc75345590501c052775867fe7e075a2d
gentile95 <lucas.gentile95@gmail.com>
2022-11-02 20:06:35 -0300

    adicionando projeto de métodos com java

---
+++ metodos/src/Usuario.java
@@ -0,0 +1,25 @@
+public class Usuario{
+  public static void main(String[] args) {
+    
+    SmartTv smartTv = new SmartTv();
+
+    System.out.println("TV ligada? " + smartTv.ligada);
+    System.out.println("Canal atual: " + smartTv.canal);
+    System.out.println("Volume: " + smartTv.volume);
+
+    smartTv.ligar();
+
+    System.out.println(smartTv.ligada);
+
+    smartTv.diminuirVolume();
+    smartTv.diminuirVolume();
+    smartTv.diminuirVolume();
+
+    System.out.println(smartTv.volume);
+
+    smartTv.mudarCanal(13);
+
+    System.out.println(smartTv.canal);
+
+  }
+}
