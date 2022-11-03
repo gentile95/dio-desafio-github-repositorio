cd048577e7e443cbe7a5ca3076fd1f3cb4da136b
gentile95 <lucas.gentile95@gmail.com>
2022-11-02 23:39:18 -0300

    Executando programa java pelo terminal

--- metodos/src/Usuario.java
+++ metodos/src/Usuario.java
@@ -2,7 +2,7 @@ public class Usuario{
   public static void main(String[] args) {
     
     SmartTv smartTv = new SmartTv();
-
+  
     System.out.println("TV ligada? " + smartTv.ligada);
     System.out.println("Canal atual: " + smartTv.canal);
     System.out.println("Volume: " + smartTv.volume);
